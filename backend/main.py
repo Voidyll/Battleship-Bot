f6c0e4f19ed1a98ef941fdbf0cb393eff9a3638f
Jonathan Taylor <mrjackedupbear@DevTwat.customerdomain.ckt.net>
2026-04-17 13:10:14 +0000

    Add API Endpoints Defined in Integration Doc
    Added 3 endpoints defined in the backend integration doc. Currently is just returning useless information, but it is a start.
    Next commit will focus on the create game method endpoint.

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from flask import Flask
+import sys
+
+sys.path.append("../")
+
+import game.game as g;
+import AI.agent as ai;
+
+app = Flask(__name__)
+
+agent = ai.Agent
+
+agent.load("../AI/checkpoints/final_model")
+
+@app.route("/api/game/new", methods=["POST"])
+def createGame():
+    game = g.Game()
+
+    agent.place_all_ships(self=agent, board=game.boards[2])
+
+    returnVal = game.to_snapshot()
+
+    return f"<p>{returnVal}</p>"
+
+@app.route("/api/game/place-ship", methods=["POST"])
+def placeShip():
+    return "<p>Ship Placed</p>"
+
+@app.route("/api/game/fire", methods=["POST"])
+def fire():
+    return "<p>Fired</p>"
+
+@app.route("/api/game/state", methods=["GET"])
+def getState():
+    return "<p>Current game state</p>"
