c21e02b093ce568ace84df156dbc033e0d173531
Jonathan Taylor <mrjackedupbear@DevTwat.customerdomain.ckt.net>
2026-04-17 18:38:12 +0000

    Remove Useless GameCreator.py File.
    Successfully set up a create game endpoint that will create a game, have the AI place the ships, and return the game snapshot to
    the requestor...

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import sys
-import createGame as createGame
 
 sys.path.append("../")
 
@@ -9,15 +8,17 @@ import AI.agent as ai;
 
 app = Flask(__name__)
 
-agent = ai.Agent
+agent = ai.Agent()
 
 agent.load("../AI/checkpoints/final_model")
 
 @app.route("/api/game/new", methods=["POST"])
 def createGame():
-    returnVal = createGame.createGame(agent)
+    game = g.Game()
 
-    return f"<p>{returnVal}</p>"
+    agent.place_all_ships(board=game.boards[2])
+
+    return game.to_snapshot()
 
 @app.route("/api/game/place-ship", methods=["POST"])
 def placeShip():
